134ea15a9c3c146c12c08009c54b8e9da4f3df54
agungdwih <agungdwi.hartanto3@gmail.com>
2023-02-14 21:57:39 +0700

    first commit

---
+++ public/assets/js/app.js
@@ -0,0 +1,38 @@
+function bayar(id) {
+    // console.log(id);
+    $.ajax({
+        type: "get",
+        url: "midtrans/"+id,
+        dataType: "json",
+        success: function (response) {
+
+            // console.log(response);
+            snap.pay(response, {
+                // Optional
+                onSuccess: function (result) {
+                    send_response(result);
+                },
+                // Optional
+                onPending: function (result) {
+                },
+                // Optional
+                onError: function (result) {
+                }
+            });
+
+        }
+    });    
+}
+
+$('#bayar').click(function (e) {
+   
+    // console.log('aaaaaaaa');
+    e.preventDefault();
+    alert('success');
+   
+});
+
+function send_response(result){
+    document.getElementById('call_json').value = JSON.stringify(result);
+    $('#submit_form').submit();
+}
